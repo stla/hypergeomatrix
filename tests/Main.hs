4636246d3e3a9d8aa8b54390cbedc872a4ef3006
stla <laurent_step@yahoo.fr>
2023-02-17 13:18:01 +0100

    removed unused variable

--- tests/Main.hs
+++ tests/Main.hs
@@ -9,7 +9,6 @@ main :: IO ()
 main = defaultMain $
   testGroup "Tests"
   [ testCase "a 2F1 value" $ do
-      let alpha = 2 :: Double
       h <- hypergeomat 10 2 [1,2] [3] [0.2, 0.5]
       assertEqual ""
         (approx 8 1.79412894456143)
